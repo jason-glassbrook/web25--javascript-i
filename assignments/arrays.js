d6075853c3083dc056d5d6144e3ca5de08dcb565
o1123516766542617 <o1123516766542617@outlook.com>
2019-10-14 17:01:56 -0400

    do arrays.js/challenge-3

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -87,10 +87,10 @@ let last_car = inventory[last_i];
 console.log(`The last car is a ${last_car.car_year} ${last_car.car_make} ${last_car.car_model}.`);
 
 // ==== Challenge 3 ====
-// The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModels = [];
-let carModelsSorted = [];
-console.log();
+// The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console.
+let car_models = inventory.map( (e) => (e.car_model) ) ;
+let car_models_sorted = car_models.sort() ;
+console.log( car_models_sorted );
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
